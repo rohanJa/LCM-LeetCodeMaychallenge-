1660a833daa690f09cc343f965275624fce9fb87
rohanJain123 <rohan.jain150300@gmail.com>
2020-05-28 19:00:17 +0530

    practice

--- practice.py
+++ practice.py
@@ -1,8 +1,42 @@
+import math
+
+s="aAfaf"
+
+print(s.index('a'))
+print(math.gcd(111111111111111,1111111111111111))
+
 print(' 2 1')
 print((' 2 '.strip()))
 print(str(bin(2))[::-1])
 print(int('1111',2))
 a=[3,12,5,53,4]
-
 a.sort()
 print(a)
+
+f={
+    1:"asas",
+    2:"3",
+    3:3
+}
+l=list(f)
+print(l)
+v=list(f.values()) #dict values stored in form of list
+print(v)
+k=list(f.keys()) #dict keys stored in form of list
+print(k)
+
+s="print rohan"
+print(s.replace("p","")) #it doesnot replace the char permanentaly we have ot store it
+print(s) # p is not removed from the string
+
+s=s.replace("p","")
+
+print(s) # s is now removed
+
+n = list(map(int,input().split()))
+
+#rotation
+a=1
+print(str[-a:]+str[:-a])
+
+index()
