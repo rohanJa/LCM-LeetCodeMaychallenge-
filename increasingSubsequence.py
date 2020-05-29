46ccd0edfee345ee9e65caccfcda640886ec1fe6
rohanJain123 <rohan.jain150300@gmail.com>
2020-05-29 17:08:50 +0530

    updated increasing longest subsequence

--- increasingSubsequence.py
+++ increasingSubsequence.py
@@ -1,12 +1,12 @@
-arr=list(map(int ,input().split()))
-max_number=-11
-l=[1 for i in range(0,len(arr))]
-for i in range(0,len(arr)):
+arr=list(map(int,input().split()))
+
+l=[1]*len(arr)
+
+for i in range(1,len(arr)):
     for j in range(0,i):
-        if arr[i] >arr[j] and l[j]+1>l[i]:
+        if arr[i]>arr[j] and l[j]+1>l[i]:
             l[i]=l[j]+1
-    print(l)
-for j in range(0,len(l)):
-    if(max_number<l[j]):
-        max_number=l[j]
-print(max_number)
+mac=-1
+for i in range(0,len(arr)):
+    mac=max(mac,l[i])
+print(mac)
