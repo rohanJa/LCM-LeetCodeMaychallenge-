33d3b9c3ceb8c8ec26f408a4bf0dec6e5370d546
rohanJa <rohan.jain150300@gmail.com>
2020-06-27 22:26:25 +0530

    Maxmex.c

---
+++ maxmex.c
@@ -0,0 +1,11 @@
+t = int(input())
+n,m = int(map(int,input().split()))
+
+arr = list(map(int,input().split()))
+mex = 1; 
+while(st.count(mex)):
+     mex+=1 
+
+if(mex != m):
+    ans = -1 
+print(ans) 
