2e3706a29ce692fbb0dc39b7831e2dd817b1c3ef
rohanJa <rohan.jain150300@gmail.com>
2020-07-16 15:51:02 +0530

    copy function

---
+++ Python/copy.py
@@ -0,0 +1,13 @@
+first = [1, 2, 3]
+copyList = first
+second = first.copy() 
+
+first.append(4)
+first.append(9)
+
+# both copyList and second will contain the copy of 'first' list
+# But differnce is that changes in 'first' is shown in copyList 
+# but if we use 'copy()' method to copy the list 'second' list
+# and no changes will be shown in the 'second list
+print("copyList "+str(copyList)) 
+print('Second '+str(second))
