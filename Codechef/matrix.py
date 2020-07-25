25de6c349abd425ff64605de1b5404e89c1f62f1
rohanJa <rohan.jain150300@gmail.com>
2020-07-25 22:11:36 +0530

    matrix

---
+++ Codechef/matrix.py
@@ -0,0 +1,25 @@
+t = int(input())
+
+while(t>0):
+    n, m, x , y =  map(int,input().split())
+    total=0
+    row = n
+    col = m
+
+    if(n%2==0 or m%2==0):
+        multi = (row*col)//2
+        if(y-x<x):
+            total+=(multi*y)
+        else:
+            total+=(row*col*(x))
+    else:
+        multi=(row*(col-1))//2
+        if(y-x<x):
+            total+=(multi*y)
+            total+=(((col)//2)*y)+(y-1) 
+        else:
+            total+=(row*col*(x))
+
+    print(total)
+
+    t-=1
