1f3ba0dc1366307330c954414af418764ed19c86
rohanJa <rohan.jain150300@gmail.com>
2020-08-18 22:31:29 +0530

    copy function in python

--- Python/copy.py
+++ Python/copy.py
@@ -4,10 +4,10 @@ second = first.copy()
 
 first.append(4)
 first.append(9)
-
+first.pop(0)
 # both copyList and second will contain the copy of 'first' list
 # But differnce is that changes in 'first' is shown in copyList 
 # but if we use 'copy()' method to copy the list 'second' list
-# and no changes will be shown in the 'second list
+# and no changes will be shown in the 'second' list
 print("copyList "+str(copyList)) 
 print('Second '+str(second))
